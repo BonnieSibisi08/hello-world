973563757de926a70d4061eb53cea6b1efc25611
BonnieSibisi08 <sibisibornface@gmail.com>
2023-07-01 19:31:48 +0200

    new changes 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-
 
 /* @NgModule({
   declarations: [
